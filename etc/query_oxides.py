5ee21b27e28f61ec93bd20e52676bba6cecae7f6
Oleg Rubel <oleg.v.rubel@gmail.com>
2021-07-22 04:13:20 +0200

    proper query

--- etc/query_oxides.py
+++ etc/query_oxides.py
@@ -1,5 +1,14 @@
-from aiida.orm import QueryBuilder, Node, SinglefileData, Dict
+from aiida.orm import QueryBuilder, Node, SinglefileData, Dict, StructureData
+from aiida.plugins.factories import CalculationFactory
 
+Wien2kRunLapw = CalculationFactory("wien2k-run_lapw")
 query = QueryBuilder()
-query.append(Node)
-query.count()
+query.append(Wien2kRunLapw, tag='calculation')
+query.append(Dict, with_incoming='calculation', project=['id'])
+query.append(StructureData, with_descendants='calculation', project=['id'])
+query.all()
+for q in query.iterall():
+    iddict, idstruct = q
+    ndict = load_node(iddict)
+    nstruct = load_node(idstruct)
+    print(nstruct.get_formula(), ndict.attributes, )
