60190da745de4f1bd3a97a447274f0d6368c2c07
Oleg Rubel <oleg.v.rubel@gmail.com>
2021-07-29 22:24:43 +0200

    data for the rest of periodic table

--- etc/query_oxides.py
+++ etc/query_oxides.py
@@ -14,3 +14,15 @@ for q in query.iterall():
     ndict = load_node(iddict)
     nstruct = load_node(idstruct)
     print(nstruct.get_formula(), ndict.attributes)
+
+query = QueryBuilder()
+query.append(Wien2kRunLapw)
+query.all()
+count_total = 0
+count_ok = 0
+for node in query.iterall():
+    print(f"run_lapw PK: {node[0].pk}, status: {node[0].process_state.value}")
+    count_total += 1
+    if( node[0].is_finished_ok ):
+        count_ok += 1
+print(f"total {count_total} processes, {count_ok} finished OK")
