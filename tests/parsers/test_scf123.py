c4f6399f0a23753f56083bbedfd9b740023bc4ad
Sebastiaan Huber <mail@sphuber.net>
2023-10-05 18:47:21 +0200

    Devops: Add unit tests for calculation and parser plugin
    Unit tests are added for the `CalcJob` and `Parser` plugins. The
    reference data for the parser tests were taken from the data of the
    AiiDA archive that was part of the AiiDA common workflow paper:
    https://archive.materialscloud.org/record/file?filename=acwf-verification_unaries-verification-PBE-v1_results_wien2k.aiida&record_id=1770
    
    The `pytest` package is used for the tests with `pytest-regressions`
    being used to compare results with reference files.

---
+++ tests/parsers/test_scf123.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""Tests for the :mod:`aiida_wien2k.parsers.scf123` module."""
+# pylint: disable=redefined-outer-name
+from aiida_wien2k.calculations.run123_lapw import Wien2kRun123Lapw
+
+
+def test_default(generate_calc_job_node, generate_parser, data_regression):
+    """Test parsing of default calculation."""
+    node = generate_calc_job_node("wien2k-run123_lapw", "scf123", "default")
+    parser = generate_parser("wien2k-scf123-parser")
+    results, calcfunction = parser.parse_from_node(node, store_provenance=False)
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_finished_ok, calcfunction.exit_message
+    data_regression.check({"scf_grep": results["scf_grep"].get_dict()})
+
+
+def test_failed_warning_converg(generate_calc_job_node, generate_parser, data_regression):
+    """Test parsing of exit code ``WARNING_CONVERG``."""
+    node = generate_calc_job_node("wien2k-run123_lapw", "scf123", "failed_warning_converg")
+    parser = generate_parser("wien2k-scf123-parser")
+    results, calcfunction = parser.parse_from_node(node, store_provenance=False)
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_failed, calcfunction.exit_status
+    assert calcfunction.exit_status == Wien2kRun123Lapw.exit_codes.WARNING_CONVERG.status
+    data_regression.check({"scf_grep": results["scf_grep"].get_dict()})
+
+
+def test_failed_warning_other(generate_calc_job_node, generate_parser, data_regression):
+    """Test parsing of exit code ``WARNING_OTHER``."""
+    node = generate_calc_job_node("wien2k-run123_lapw", "scf123", "failed_warning_other")
+    parser = generate_parser("wien2k-scf123-parser")
+    results, calcfunction = parser.parse_from_node(node, store_provenance=False)
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_failed, calcfunction.exit_status
+    assert calcfunction.exit_status == Wien2kRun123Lapw.exit_codes.WARNING_OTHER.status
+    data_regression.check({"scf_grep": results["scf_grep"].get_dict()})
+
+
+def test_failed_warning_qtl_b(generate_calc_job_node, generate_parser, data_regression):
+    """Test parsing of exit code ``WARNING_QTL_B``."""
+    node = generate_calc_job_node("wien2k-run123_lapw", "scf123", "failed_warning_qtl_b")
+    parser = generate_parser("wien2k-scf123-parser")
+    results, calcfunction = parser.parse_from_node(node, store_provenance=False)
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_failed, calcfunction.exit_status
+    assert calcfunction.exit_status == Wien2kRun123Lapw.exit_codes.WARNING_QTL_B.status
+    data_regression.check({"scf_grep": results["scf_grep"].get_dict()})
